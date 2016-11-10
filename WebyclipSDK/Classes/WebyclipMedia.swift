8ccadc2567aae626a125de46a3504048ab21de85
namingray <namingray@gmail.com>
2016-11-10 16:50:32 +0200

    Added fallback for context creation

--- WebyclipSDK/Classes/WebyclipMedia.swift
+++ WebyclipSDK/Classes/WebyclipMedia.swift
@@ -1,23 +1,41 @@
-
-
-import Foundation
+import SwiftyJSON
 
 /**
  Represents a media object in Webyclip context.
 */
 open class WebyclipMedia {
+    //MARK: - Public
+    
     /// The name of the media provider
-    open var providerName: String!
+    open var providerName: String
     /// The id of the media
-    open var mediaId: String!
+    open var mediaId: String
     /// The media title
-    open var title: String!
+    open var title: String
     /// The media author
-    open var author: String!
+    open var author: String
     /// The media publish date
-    open var publishDate: Date!
+    open var publishDate: Date
     /// A list of related items associated with the media
     open var relatedItems: [WebyclipRelatedItem] = []
+    /// A media duration
+    open var duration: Int
     
-    open var duration: Int!
+    /**
+     Initializes WebyclipMedia
+     */
+    public init(media: JSON) {
+        
+        self.mediaId = media["external_id"].stringValue
+        self.providerName = media["provider"].stringValue
+        
+        let baseMedia = media["base_media"]
+        
+        self.title = baseMedia["provider_title"].stringValue
+        self.author = baseMedia["channel_name"].stringValue
+        self.publishDate = NSDate(timeIntervalSince1970: baseMedia["published_at"].doubleValue / 1000) as Date
+        self.duration = baseMedia["duration"].intValue
+        
+        self.relatedItems = []
+    }
 }
--- WebyclipSDK/Classes/WebyclipSession.swift
+++ WebyclipSDK/Classes/WebyclipSession.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 import Alamofire
 import CryptoSwift
@@ -8,9 +7,15 @@ import SwiftyJSON
  A session is user's interaction with the Webyclip videos
 */
 open class WebyclipSession {
-    
-    open var siteId: String
+    //MARK: - Private
     fileprivate var sslEndpoint: String
+    
+    private func getDataFromServer() {
+        
+    }
+    
+    //MARK: - Public
+    open var siteId: String
     
     public init(siteId: String) {
         let components = siteId.components(separatedBy: ":")
@@ -28,33 +33,28 @@ open class WebyclipSession {
     open func createContext(_ config: WebyclipContextConfig, success: @escaping (_ context: WebyclipContext) -> Void, error: @escaping (_ error: NSError?) -> Void) {
         
         let md5 = config.id.md5().md5()
-    
         let url = "https://\(self.sslEndpoint)/group/\(md5).json"
         
         Alamofire.request(url).responseString { response in
-            if response.result.isSuccess {
-                let value = response.result.value!
-                let jsonString = value.substring(with: Range<String.Index>(uncheckedBounds: (lower: value.index(value.startIndex, offsetBy: 24) , upper: value.index(value.endIndex, offsetBy: -1))))
-                var webyclipMedia = [WebyclipMedia]()
-                if let dataFromString = jsonString.data(using: String.Encoding.utf8, allowLossyConversion: false) {
-                    let json = JSON(data: dataFromString)
-                    for item in json["contexts"][0]["medias"].arrayValue {
-                        let mediaItem = WebyclipMedia()
-                        mediaItem.mediaId = item["external_id"].stringValue
-                        mediaItem.providerName = item["provider"].stringValue
-                        mediaItem.relatedItems = []
-                        mediaItem.title = item["base_media"]["provider_title"].stringValue
-                        mediaItem.author = item["base_media"]["channel_name"].stringValue
-                        mediaItem.publishDate = NSDate(timeIntervalSince1970: item["base_media"]["published_at"].doubleValue / 1000) as Date!
-                        mediaItem.duration = item["base_media"]["duration"].intValue
-                        webyclipMedia.append(mediaItem)
-                        
-                    }
-                    success(WebyclipContext(items: webyclipMedia))
-                }
+            guard response.result.isSuccess else {
+                print("Error while fetching context: \(response.result.error)")
+                self.getDataFromServer()
+                return
             }
-            else {
-                error(response.result.error as NSError?)
+            guard let value = response.result.value, value.range(of: "webyclipMediaForContext") != nil else {
+                print("Malformed data received from CDN")
+                self.getDataFromServer()
+                return
+            }
+            let jsonString = value.substring(with: Range<String.Index>(uncheckedBounds: (lower: value.index(value.startIndex, offsetBy: 24) , upper: value.index(value.endIndex, offsetBy: -1))))
+            var webyclipMedia = [WebyclipMedia]()
+            if let dataFromString = jsonString.data(using: String.Encoding.utf8, allowLossyConversion: false) {
+                let json = JSON(data: dataFromString)
+                for item in json["contexts"][0]["medias"].arrayValue {
+                    let mediaItem = WebyclipMedia(media: item)
+                    webyclipMedia.append(mediaItem)
+                }
+                success(WebyclipContext(items: webyclipMedia))
             }
         }
     }
